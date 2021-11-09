f0614c2b8a2c8afcbaf25bd4e7b1679d9a2ab4cd
lina834 <nay8649@gamil.com>
2021-11-08 21:38:48 -0600

    change in nav

--- src/Menu/UserPlaylist.js
+++ src/Menu/UserPlaylist.js
@@ -17,15 +17,15 @@ const UserPlaylist = () => {
     if (doc.exists) {
 
         var html = "<div class='wrapper'><h1 id='MenuTitle'>Liked Movies</h1>"
-        for (var i = 0; i < doc.data().Rated.liked.length; i++) {
-            html += "<img src=" + doc.data().Rated.liked[i].moviePoster + "/>"
+        for (var i = 0; i < doc.data().Rated?.liked?.length; i++) {
+            html += "<img src=" + doc.data().Rated?.liked[i].moviePoster + "/>"
         }
         html += "<h1 id='MenuTitle'>DisLiked Movies</h1>"
-        for (var i = 0; i < doc.data().Rated.disliked.length; i++) {
+        for (var i = 0; i < doc.data().Rated?.disliked?.length; i++) {
             html += "<img src=" + doc.data().Rated.disliked[i].moviePoster + "/>"
         }
         html += "<h1 id='MenuTitle'>Added Movies</h1>"
-        for (var i = 0; i < doc.data().Added.defaultPlaylist.length; i++) {
+        for (var i = 0; i < doc.data().Added?.defaultPlaylist?.length; i++) {
             html += "<img src=" + doc.data().Added.defaultPlaylist[i].moviePoster + "/>"
         }
         html += "</div>"
--- src/Navbar/NavbarElements.js
+++ src/Navbar/NavbarElements.js
@@ -1,9 +1,30 @@
-import React from 'react';
+// import React from 'react';
+import React, {useRef, useState,useEffect} from 'react';
+
+import SignIn from '../SignIn';
+import SignOut from '../SignOut';
+import RateMovies from '../RateMovies';
+import Menu from '../Menu/Menu';
+import UserPlaylist from '../Menu/UserPlaylist';
+import SuggestedMovies from '../Menu/SuggestedMovies';
+import '../Menu/Menu.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+const auth = firebase.auth();
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
   return(
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">NextFlix</a>
+        <a class="navbar-brand text-danger font-weight-bold" href="#"><span className="h2">N</span>extFlex</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -34,10 +55,32 @@ const Navbar = () => {
             </li> */}
           </ul>
 
-          <form class="form-inline my-2 my-lg-0" method="POST" action="SearchBar.js">
+          {/* <section>
+            <Switch>
+              <Route exact path="/">
+                {user ? <RateMovies /> : <SignIn />}
+              </Route>
+              <Route exact path="/Menu">
+                {user ? <Menu /> : <SignIn />}
+              </Route>
+              <Route exact path="/UserPlaylist">
+                {user ? <UserPlaylist /> : <SignIn />}
+                <div id = "playlist"></div>
+              </Route>
+              <Route exact path="/SuggestedMovies">
+                {user ? <SuggestedMovies /> : <SignIn />}
+              </Route>
+            </Switch>
+          </section> */}
+
+
+          {/* <form class="form-inline my-2 my-lg-0" method="POST" action="SearchBar.js">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          </form> */}
+
+          <SignOut />
+
         </div>
     </nav>
   );
