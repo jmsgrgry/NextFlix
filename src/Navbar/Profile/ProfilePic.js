91d82824e90113272c23d0b247418610c9d3a320
James Gregory <jbgregory@wustl.edu>
2021-11-30 18:35:36 -0600

    changes

--- src/Navbar/Profile/ProfilePic.js
+++ src/Navbar/Profile/ProfilePic.js
@@ -1,44 +1,50 @@
 import React, { useEffect } from 'react';
 import '../../App.css';
-
 import firebase from 'firebase/compat/app';
 import "firebase/compat/storage";
 import 'firebase/database';
 import 'firebase/firestore';
 import '../../Menu/Menu.css';
-import { initializeApp } from "firebase/app";
 
 const auth = firebase.auth();
 
 function ProfilePic() {
 
+    const [file, setFile] = React.useState(null);
+    const [url, setURL] = React.useState("");
 
-    const [image , setImage] = React.useState('');
-
-    const upload = ()=>{
-    if (image == null) return;
-        if (!firebase.storage) {
-            console.log("stopped");
-            return;
-        }
-
-        console.log(image);
-
-        const ref = firebase.storage().ref(`images/${image.name}`);
-
-        // const ref = firebase.storage.ref(`images/${image.name}`);
-        ref.put(image).on("state_changed" , alert("success") , alert);
-        
+    function handleChange(e) {
+        setFile(e.target.files[0]);
     }
-  
-  return (
-    <div className="App">
-      <center>
-      <input type="file" onChange={(e)=>{setImage(e.target.files[0])}}/>
-      <button onClick={upload}>Upload</button>
-      </center>
-    </div>
-  );
-}
+
+    function handleUpload(e) {
+        e.preventDefault();
+        const ref = firebase.storage().ref(`images/${file.name}`);
+        const uploadTask = ref?.put(file);
+
+        var uploadedImage = URL.createObjectURL(file);
+        console.log(uploadedImage);
+
+        uploadTask?.on("state_changed", console.log, console.error, () => {
+        ref
+            .getDownloadURL()
+            .then((url) => {
+            setFile(null);
+            setURL(url);
+            });
+        });
+    }
+
+    return (
+        <div>
+        <form onSubmit={handleUpload}>
+            <input type="file" onChange={handleChange} />
+            <button disabled={!file}>Set Profile Pic</button>
+        </form>
+        <br/>
+        <img class="profile-image" src={url}/>
+        </div>
+    );
+  }
 
 export default ProfilePic;
